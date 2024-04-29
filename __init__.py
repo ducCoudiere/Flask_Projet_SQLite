0e89b16278d64026fbb297185397bb9217fa5994
ducCoudiere <165167098+ducCoudiere@users.noreply.github.com>
2024-04-29 21:16:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,5 +77,15 @@ def enregistrer_client():
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
+@app.route('/fiche_nom/<post_str>')
+def ReadficheN(post_str):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_str,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 if __name__ == "__main__":
   app.run(debug=True)
