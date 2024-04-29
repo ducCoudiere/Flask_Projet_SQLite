9ddb43d4889273de51d8e87c9c679ffd0f8914c0
ducCoudiere <165167098+ducCoudiere@users.noreply.github.com>
2024-04-29 15:19:00 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
 from flask import render_template
-from flask import json
+from flask import json  
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
